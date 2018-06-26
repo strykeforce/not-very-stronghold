7c67fd57abce84e59943757e4f65879d2eaef1a4
Marisa Witcpalek <mwitcpalek@gmail.com>
2018-06-26 17:56:16 -0400

    Initial setup of repo

---
+++ src/main/java/frc/team2767/Robot.java
@@ -0,0 +1,12 @@
+package org.strykeforce.quickstart;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+  @Override
+  public void robotInit() {
+    System.out.println("Hello!");
+  }
+
+}
